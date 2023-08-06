eab75ded990a0f1faa62b9766c969c4838e018d2
simpleneeraj <teamsimpleneeraj@gmail.com>
2023-08-07 00:33:10 +0530

    update

---
+++ git-cmd.md
@@ -0,0 +1,5 @@
+<!-- For Code Push -->
+
+```bash
+git add . && git commit -m "update" && git pull && git push
+```
--- providers/safe-area.tsx
+++ providers/safe-area.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { app } from '@/styles/app';
-import { StatusBar } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const SafeAreaProvider = ({ children }: React.PropsWithChildren<{}>) => {
   return (
     <SafeAreaView style={app['app-container']}>
-      <StatusBar barStyle="light-content" />
+      <StatusBar style="inverted" />
       {children}
     </SafeAreaView>
   );
