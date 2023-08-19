35d43b5c908d1fc16f780feb8f945579976682a7
simpleneeraj <teamsimpleneeraj@gmail.com>
2023-08-19 21:58:35 +0530

    update

--- app/product-selection/contact/support/new-complaints.tsx
+++ app/product-selection/contact/support/new-complaints.tsx
@@ -1,52 +1,80 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  ScrollView,
-  Image,
-  Button,
-  TouchableOpacity,
-} from "react-native";
-import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
-import { useRouter } from "expo-router";
-function NewComplain({ navigation }:any) {
-    const router = useRouter();
+import React from 'react';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import useStorageData from '@/apis/hooks/use-storage-data';
+import useInputs from '@/hooks/use-inputs';
+import createComplaints from '@/apis/mutations/contact/complaints';
+import { ScrollView } from 'react-native-gesture-handler';
+
+const complains = [
+  {
+    name: 'Payment not working',
+  },
+  {
+    name: 'App is too slow',
+  },
+  {
+    name: 'Neee help',
+  },
+  {
+    name: 'App is crashing',
+  },
+];
+
+function NewComplain() {
+  const [selectedComplain, setSelectedComplain] = React.useState(
+    complains[0].name
+  );
+
+  const router = useRouter();
+  const { user } = useStorageData('user');
+
+  const [state, setState] = useInputs({
+    name: '',
+    email: '',
+    message: '',
+    mobileNumber: '',
+  });
+
+  const onSubmit = React.useCallback(async () => {
+    if (user?.token) {
+      try {
+        const payload = {};
+        const { data, status } = await createComplaints(payload, user?.token);
+        console.log('Response from Coplaint ', data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [state, user?.token]);
+
   return (
-    <>
-      <View style={{ display: "flex", flex: 1, backgroundColor: "white" }}>
+    <ScrollView>
+      <View style={{ display: 'flex', flex: 1, backgroundColor: 'white' }}>
         <View
           style={{
             marginLeft: 18,
             marginTop: 17,
-            flexDirection: "row",
-            alignItems: "center",
-            
+            flexDirection: 'row',
+            alignItems: 'center',
           }}
         >
           <Image
-                      source={require("@/assets/icons/arrow_back.png")}
-
+            source={require('@/assets/icons/arrow_back.png')}
             style={{
               width: 16.03,
               height: 16.03,
               marginRight: 35.98,
-              
-             
             }}
           />
           <Text
             style={{
               fontSize: 20,
-              fontWeight: "500",
+              fontWeight: '500',
               lineHeight: 32,
               letterSpacing: 0.5,
-              color: "#4B5050",
-              
+              color: '#4B5050',
             }}
           >
             New Complain
@@ -54,7 +82,7 @@ function NewComplain({ navigation }:any) {
         </View>
 
         <Image
-          source={require("@/assets/onboarding/payrowLogo.png")}
+          source={require('@/assets/onboarding/payrowLogo.png')}
           style={{
             width: 150,
             height: 48.529,
@@ -67,173 +95,86 @@ function NewComplain({ navigation }:any) {
           style={{
             marginLeft: 108,
             marginRight: 108,
-            textAlign: "center",
+            textAlign: 'center',
             fontSize: 14,
-            fontWeight: 400,
+            fontWeight: '400',
             lineHeight: 20,
             letterSpacing: 0.25,
-            marginTop:24.47,
-            color: "#4C4C4C",
+            marginTop: 24.47,
+            color: '#4C4C4C',
           }}
         >
           Select Complaint Type
         </Text>
-        <View style={{ flexDirection: "column", alignItems: "center" }}>
-          <View
-            style={{
-              width: 296,
-              height: 48,
-              marginTop: 16,
-              marginLeft: 32,
-              marginRight: 32,
-              paddingTop: 15,
-              paddingLeft: 16,
-              paddingRight: 145,
-              paddingBottom: 13,
-              borderWidth: 1,
-              borderStyle: "solid",
-              borderColor: "rgba(75, 80, 80, 0.20)",
-              borderRadius: 8,
-              marginBottom:16
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 14,
-                lineHeight: 20,
-                fontWeight: "500",
-                letterSpacing: 0.1,
-                width: 155,
-                height: 20,
-                color: "rgba(75, 80, 80, 0.80)",
-              }}
-            >
-              Payment not working
-            </Text>
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
-              width: 296,
-              height: 48,
-              marginTop: 16,
-              marginLeft: 32,
-              marginRight: 32,
-              paddingTop: 15,
-              paddingLeft: 16,
-              paddingRight: 145,
-              paddingBottom: 13,
-              borderWidth: 1,
-              borderStyle: "solid",
-              borderColor: "rgba(75, 80, 80, 0.20)",
-              borderRadius: 8,
-              gap: 90,
-              marginBottom:16
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 14,
-                lineHeight: 20,
-                fontWeight: "500",
-                letterSpacing: 0.1,
-                color: "rgba(75, 80, 80, 0.80)",
-                width: 155,
-                height: 20,
-              }}
-            >
-              App is too slow
-            </Text>
-            <View>
-              <MaterialCommunityIcons
-                name="checkbox-marked-circle"
-                size={20}
-                color="#4B5050E5"
-                style={{ width: 24, height: 24 }}
-              />
-            </View>
-          </View>
-          <View
-            style={{
-              width: 296,
-              height: 48,
-              marginTop: 16,
-              marginLeft: 32,
-              marginRight: 32,
-              paddingTop: 15,
-              paddingLeft: 16,
-              paddingRight: 145,
-              paddingBottom: 13,
-              borderWidth: 1,
-              borderStyle: "solid",
-              borderColor: "#4B5050",
-              borderRadius: 8,
-              borderColor: "rgba(75, 80, 80, 0.20)",
-              marginBottom:16
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 14,
-                lineHeight: 20,
-                fontWeight: "500",
-                letterSpacing: 0.1,
-                color: "rgba(75, 80, 80, 0.80)",
-                width: 155,
-                height: 20,
-              }}
-            >
-              Need help
-            </Text>
-          </View>
-          <View
-            style={{
-              width: 296,
-              height: 48,
-              marginTop: 16,
-              marginLeft: 32,
-              marginRight: 32,
-              paddingTop: 15,
-              paddingLeft: 16,
-              paddingRight: 145,
-              paddingBottom: 13,
-              borderWidth: 1,
-              borderStyle: "solid",
-              borderColor: "rgba(75, 80, 80, 0.20)",
-              borderRadius: 8,
-              marginBottom:16
-      
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 14,
-                lineHeight: 20,
-                fontWeight: "500",
-                letterSpacing: 0.1,
-                color: "rgba(75, 80, 80, 0.80)",
-                width: 155,
-                height: 20,
-              }}
-            >
-              App is crashing
-            </Text>
-          </View>
+        <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+          {complains.map((item, index) => {
+            return (
+              <TouchableOpacity
+                key={index}
+                onPress={() => setSelectedComplain(item.name)}
+              >
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    width: 296,
+                    height: 48,
+                    marginTop: 16,
+                    marginLeft: 32,
+                    marginRight: 32,
+                    paddingTop: 15,
+                    paddingLeft: 16,
+                    paddingRight: 145,
+                    paddingBottom: 13,
+                    borderWidth: 1,
+                    borderStyle: 'solid',
+                    borderColor: 'rgba(75, 80, 80, 0.20)',
+                    borderRadius: 8,
+                    gap: 90,
+                    marginBottom: 16,
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      lineHeight: 20,
+                      fontWeight: '500',
+                      letterSpacing: 0.1,
+                      color: 'rgba(75, 80, 80, 0.80)',
+                      width: 155,
+                      height: 20,
+                    }}
+                  >
+                    {item.name}
+                  </Text>
+                  {item.name === selectedComplain && (
+                    <View>
+                      <MaterialCommunityIcons
+                        name="checkbox-marked-circle"
+                        size={20}
+                        color="#4B5050E5"
+                        style={{ width: 24, height: 24 }}
+                      />
+                    </View>
+                  )}
+                </View>
+              </TouchableOpacity>
+            );
+          })}
         </View>
-        <View style={{ flexDirection: "col", alignItems: "center" }}>
+        <View style={{ flexDirection: 'column', alignItems: 'center' }}>
           <Text
             style={{
               marginLeft: 128,
               marginRight: 128,
-              textAlign: "center",
+              textAlign: 'center',
               fontSize: 14,
-              fontWeight: 400,
+              fontWeight: '400',
               lineHeight: 20,
               letterSpacing: 0.25,
               marginTop: 32,
-              width: "auto",
+              width: 'auto',
               height: 20,
-              color: "#4C4C4C",
+              color: '#4C4C4C',
             }}
           >
             Write Complaint
@@ -243,13 +184,13 @@ function NewComplain({ navigation }:any) {
               marginLeft: 32,
               marginRight: 32,
               fontSize: 12,
-              fontWeight: 400,
+              fontWeight: '400',
               lineHeight: 12,
               letterSpacing: 0.25,
               marginTop: 30,
               width: 296,
               height: 13,
-              color: "rgba(75, 80, 80, 0.70)",
+              color: 'rgba(75, 80, 80, 0.70)',
             }}
           >
             Brief your complaint
@@ -259,11 +200,11 @@ function NewComplain({ navigation }:any) {
               marginLeft: 32,
               marginRight: 32,
               fontSize: 16,
-              fontWeight: 400,
+              fontWeight: '400',
               lineHeight: 18,
               width: 296,
               height: 54,
-              color: "rgba(75, 80, 80, 0.70)",
+              color: 'rgba(75, 80, 80, 0.70)',
               marginBottom: 8,
             }}
           >
@@ -271,7 +212,7 @@ function NewComplain({ navigation }:any) {
             industry. Lorem Ipsum has been the industry.
           </Text>
           <Image
-          source={require("@/assets/icons/sixline.png")}
+            source={require('@/assets/icons/sixline.png')}
             style={{
               marginLeft: 32,
               marginRight: 32,
@@ -283,33 +224,34 @@ function NewComplain({ navigation }:any) {
           />
           <TouchableOpacity
             onPress={() => {
-            //   navigation.navigate("registercomplain");
-            router.push("/product-selection/contact/support/register-complain")
+              //   navigation.navigate("registercomplain");
+              router.push(
+                '/product-selection/contact/support/register-complain'
+              );
             }}
           >
             <View
               style={{
-                flexDirection: "row",
-                backgroundColor: "#4B5050",
+                flexDirection: 'row',
+                backgroundColor: '#4B5050',
                 width: 296,
                 height: 48,
                 gap: 181,
                 marginRight: 32,
                 marginLeft: 32,
                 borderRadius: 8,
-                alignItems: "center",
-                justifyContent: "center",
-        
+                alignItems: 'center',
+                justifyContent: 'center',
               }}
             >
               <Text
                 style={{
                   fontSize: 16,
                   paddingLeft: 16,
-                  fontWeight: "500",
+                  fontWeight: '500',
                   lineHeight: 24,
-                  justifyContent: "center",
-                  color: "white",
+                  justifyContent: 'center',
+                  color: 'white',
                   letterSpacing: 0.1,
                 }}
               >
@@ -317,8 +259,8 @@ function NewComplain({ navigation }:any) {
               </Text>
               <View
                 style={{
-                  justifyContent: "center",
-                  alignItems: "center",
+                  justifyContent: 'center',
+                  alignItems: 'center',
                   marginRight: 16,
                 }}
               >
@@ -330,28 +272,28 @@ function NewComplain({ navigation }:any) {
 
         <View
           style={{
-            backgroundColor: "white",
-            flexDirection: "row",
-            justifyContent: "center",
+            backgroundColor: 'white',
+            flexDirection: 'row',
+            justifyContent: 'center',
           }}
         >
           <Text
             style={{
               fontSize: 14,
-              backgroundColor: "white",
-              color: "#7f7f7f",
+              backgroundColor: 'white',
+              color: '#7f7f7f',
               width: 288,
               height: 20,
-              fontWeight: "400",
-              marginTop:27,
-              opacity: 0.800000011920929
+              fontWeight: '400',
+              marginTop: 27,
+              opacity: 0.800000011920929,
             }}
           >
             ©2022 PayRow Company. All rights reserved
           </Text>
         </View>
       </View>
-    </>
+    </ScrollView>
   );
 }
 
